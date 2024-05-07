19a296c831c8fe70d564b89d51a65fac053d6822
rupomehsan <rupomehsan34@gmail.com>
2024-05-07 16:52:55 +0600

    loan test doen

--- resources/js/backend/views/pages/bm_manager/management/account_category/All.vue
+++ resources/js/backend/views/pages/bm_manager/management/account_category/All.vue
@@ -30,6 +30,33 @@
                                     class="form-control border border-info" />
                             </form>
                         </div>
+                        <div class="btns d-flex gap-2 align-items-center">
+                            <div class="table_actions">
+                                <a @click.prevent="" href="#" class="btn px-3 btn-outline-secondary"><i
+                                        class="fa fa-list"></i></a>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <i class="fa-regular fa-hand-point-right"></i>
+                                            Export All
+                                        </a>
+                                    </li>
+                                    <!---->
+                                    <li>
+                                        <a href="#/user/import" class="">
+                                            <i class="fa-regular fa-hand-point-right"></i>
+                                            Import
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" title="display data that has been deactivated" class="d-flex">
+                                            <i class="fa-regular fa-hand-point-right"></i>
+                                            Deactivated data
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
 
                     </div>
                     <div class="table-responsive card-body text-nowrap">
